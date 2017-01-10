581fb13f389c1af2d93801e4d5aa25c7d1bce8ce
bernardkkt <kianshunkin@gmail.com>
2017-01-10 22:36:12 +0800

    BASE

--- startup.sh
+++ startup.sh
@@ -1,1 +1,20 @@
-
+#!/bin/bash
+echo
+echo Logging application starting up...
+echo
+echo Retrieving real time from GPS...
+gmode=$(gpspipe -w | head -4 | egrep -o '"mode":+[0-9]' | grep -Po  '"mode":\K[^ ]+')
+while [ "$gmode" = "1" ]; do
+	echo Still attempting...
+	gmode=$(gpspipe -w | head -4 | egrep -o '"mode":+[0-9]' | grep -Po  '"mode":\K[^ ]+')
+	sleep 1
+done
+echo Changing system time...
+sudo killall gpsd
+sudo ./gpsdate /dev/ttyS0
+echo
+echo Success!
+sudo gpsd /dev/ttyS0 -n
+echo The script will now proceed to logging task.
+cd LOGGING
+sudo python mainlog.py
