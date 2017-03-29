eb5625ae5f1f318d7354773b5a618007effb986b
bernardkkt <kianshunkin@gmail.com>
2017-03-30 01:12:56 +0800

    Insert Corrected CO2 (Pressure) Value

--- logger.py
+++ logger.py
@@ -12,39 +12,39 @@ class Sensors:
 	def __init__(self):
 		try:
 			mark = 1
-			
+
 			self.pi = pigpio.pi()
 			self.k30 = self.pi.i2c_open(1, 0x68)
 			mark = mark + 1
-			
+
 			self.sht71 = SHT1x(tempdataPin, tempclkPin, SHT1x.GPIO_BOARD)
 			mark = mark + 1
-			
+
 			self.bsense = BMP180.BMP085()
 		except:
 			print "Unsuccessful initialisation. Error code: " + str(mark)
 			if mark is not 1:
 				self.pi.stop()
 			sys.exit(0)
-			
+
 	def startThreads(self):
 		try:
 			th1 = threading.Thread(target=gCO2, args=(self.pi, self.k30,))
 			th2 = threading.Thread(target=gBaro, args=(self.bsense,))
 			th3 = threading.Thread(target=gSHT, args=(self.sht71,))
-			
+
 			th2.start()
 			th2.join()
 			th1.start()
 			th3.start()
-			
+
 			th1.join()
 			th3.join()
 		except:
 			print "Unsuccessful calling. Exiting..."
 			self.pi.stop()
 			sys.exit(0)
-			
+
 	def cleanExit(self):
 			self.pi.stop()
 			sys.exit(0)
@@ -54,13 +54,17 @@ def gCO2(mano, enu):
 		mano.i2c_write_device(enu, ins2rd)
 		time.sleep(0.02)
 		cnt, taf = mano.i2c_read_device(enu, 4)
+
 		if taf[3] == ((taf[0]+taf[1]+taf[2])%256):
 			ccVal = (taf[1]*256) + taf[2]
 		else:
 			ccVal = "N/A"
+
+		if ccVal > 10000:
+			ccVal = "N/A"
 	except:
 		ccVal = "N/A"
-		
+
 	global sensorData
 	sensorData[0] = ccVal
 
@@ -71,7 +75,7 @@ def gSHT(mano):
 	except:
 		tempval = "N/A"
 		humdval = "N/A"
-		
+
 	global sensorData
 	sensorData[1] = tempval
 	sensorData[2] = humdval
@@ -83,7 +87,7 @@ def gBaro(mano):
 	except:
 		bpa = "N/A"
 		btp = "N/A"
-		
+
 	global sensorData
 	sensorData[3] = bpa
 	sensorData[4] = btp
@@ -92,21 +96,30 @@ def checkFile():
 	dia = time.strftime("%d-%m-%Y") + ".csv"
 	csvf = open(dia, "a+")
 	if os.stat(dia).st_size == 0:
-	  csvf.write("Date and Time,Temperature,BMPTemperature,Humidity,Pressure,CO2\n")
+	  csvf.write("Date and Time,Temperature,BMPTemperature,Humidity,Pressure,CO2,Corrected CO2\n")
 	  csvf.flush()
 	return csvf
+
+def correct():
+	global sensorData
+	if sensorData[0] != "N/A":
+		correctedVal = sensorData[0] / (sensorData[3] * ((4.026*0.001) + (5.780 * 0.00001 * sensorData[3])))
+		correctedVal = -(-correctedVal // 1)
+	else:
+		correctedVal = "N/A"
+	return correctedVal
 
 def main():
 	#Initialisation
 	global sensorData
 	allsensors = Sensors()
 	selfil = checkFile()
-	
+
 	#Initiate with start time
 	nt = int(time.time())
 	print "\n" + "Current start time: " + time.ctime(nt) + "\n"
-	
-	#Sampling and logging	
+
+	#Sampling and logging
 	try:
 		while True:
 			ct = int(time.time())
@@ -115,13 +128,14 @@ def main():
 				timestamp = time.ctime(ct)
 				spt = timestamp.split()
 				allsensors.startThreads()
-				
-				twrite = "Temperature in C: " + str(sensorData[1]) + "\n" + "Temperature (by BMP) in C: " + str(sensorData[4]) + "\n" + "Humidity in %: " + str(sensorData[2]) + "\n" + "Pressure in Pa: " + str(sensorData[3]) + "\n" + "CO2 concentration in ppm: " + str(sensorData[0])
-				
-				pwrite = time.strftime("%d-%m-%Y") + " " + spt[3] + "," + str(sensorData[1]) + "," + str(sensorData[4]) + "," + str(sensorData[2]) + "," + str(sensorData[3]) + "," + str(sensorData[0]) + "\n"
-				
+				pcomppm = correct()
+
+				twrite = "Temperature in C: " + str(sensorData[1]) + "\n" + "Temperature (by BMP) in C: " + str(sensorData[4]) + "\n" + "Humidity in %: " + str(sensorData[2]) + "\n" + "Pressure in Pa: " + str(sensorData[3]) + "\n" + "CO2 concentration in ppm: " + str(sensorData[0]) + "Corrected CO2 ppm: " + str(pcomppm)
+
+				pwrite = time.strftime("%d-%m-%Y") + " " + spt[3] + "," + str(sensorData[1]) + "," + str(sensorData[4]) + "," + str(sensorData[2]) + "," + str(sensorData[3]) + "," + str(sensorData[0]) + "," + str(pcomppm) + "\n"
+
 				selfil.write(pwrite)
-				selfil.flush()				
+				selfil.flush()
 				print twrite
 				print "Data logged at: " + str(spt[3]) + "\n"
 			time.sleep(0.000001)
