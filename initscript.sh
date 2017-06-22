44e75adc43cb4b2e45e48e136f55c144f854ab06
bernardkkt <kianshunkin@gmail.com>
2017-06-22 23:15:27 +0800

    Updated file

--- initscript.sh
+++ initscript.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
-echo "enable_uart=1" >> /boot/config.txt
-echo "dtparam=i2c_arm_baudrate=8000" >> /boot/config.txt
+cd /home/pi
+cp /boot/config.txt tmpp1.txt
+echo "enable_uart=1" >> tmpp1.txt
+echo "dtparam=i2c_arm_baudrate=8000" >> tmpp1.txt
+sudo mv tmpp1.txt /boot/config.txt
+sudo apt update
+sudo apt install xrdp tightvncserver gpsd-clients python-dev -y
+git clone https://bitbucket.org/lunobili/rpisht1x.git
+git clone https://github.com/adafruit/Adafruit_Python_BMP.git
