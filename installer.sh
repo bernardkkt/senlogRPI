dba4a1e63630c36fe7be66a500d04d25f6ffcd0f
bernardkkt <kianshunkin@gmail.com>
2017-07-06 06:48:55 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,7 +8,7 @@ if [ "`tail -n1 /etc/rc.local`" = "exit 0" ]
 then
 	echo -e "The script is ready to proceed."
 else
-	echo "The script cannot proceed to the next step. Please contact \nyour software support team."
+	echo -e "The script cannot proceed to the next step. Please contact \nyour software support team."
 	exit
 fi
 echo Modifying...
@@ -16,4 +16,14 @@ sudo cp /etc/rc.local /etc/rc.local.bak
 cp /etc/rc.local rc.local
 sed -i '$i \sudo bash /home/pi/senlogRPI/startup.sh &' rc.local
 sudo mv -f rc.local /etc/rc.local
-echo Success! Exiting...
+echo Compiling executable...
+sudo apt-get install libbluetooth-dev -y
+gcc senlogRPI/ipInformer.c -lbluetooth -o senlogRPI/ipInformer
+if [ -e senlogRPI/ipInformer ]
+then
+	echo -e "Blutooth name changing utility is now ready."
+else
+	echo -e "Error: Unable to create executable file. Please contact \nyour software support team."
+	exit
+fi
+echo Exiting...
