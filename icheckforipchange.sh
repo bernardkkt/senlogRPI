629a1ec4a780f411f59603bd9325608907b37553
bernardkkt <kianshunkin@gmail.com>
2017-07-06 08:05:00 +0000

    Update icheckforipchange.sh

--- icheckforipchange.sh
+++ icheckforipchange.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-cd /home/pi/senlogRPI/httpServ
-python -m CGIHTTPServer 8080 &
-cd ..
 function getName {
 	currentIP=$(ifconfig wlan | sed -n 's/.*inet addr:\(.*\)Bcast:.*/\1/p')
 	nome="DevIP: ${currentIP// }"
