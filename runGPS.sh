001229c8e960fb36c5bba21ce77f83dc362a6c7d
bernardkkt <kianshunkin@gmail.com>
2017-07-21 12:00:17 +0800

    Update runGPS.sh

--- runGPS.sh
+++ runGPS.sh
@@ -3,6 +3,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 echo
 echo Logging application starting up...
 echo
+sudo gpsd /dev/ttyS0 -n
 echo Retrieving real time from GPS...
 gmode=$(gpspipe -w -n 5 | sed -n -e 's/.*mode":\(.*\),"time.*/\1/p' | sed -n 1p)
 until [[ "$gmode" = "2" ]] || [[ "$gmode" = "3" ]]; do
