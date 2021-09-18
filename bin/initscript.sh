38121d74a2cec45028a7baebe3e068b2010b61ad
KKT <kkt@ubuntu.ubuntu-domain>
2021-09-18 10:08:36 +0800

    WORK 1

--- bin/initscript.sh
+++ bin/initscript.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+
+export DEBIAN_FRONTEND="noninteractive"
+# This script configures Raspbian to work properly with the sensors.
+# Run this script after the fresh installation of Raspbian OS to RPI.
+
 cd /home/pi
 cp /boot/config.txt tmpp1.txt
 echo "enable_uart=1" >> tmpp1.txt
 echo "dtparam=i2c_arm_baudrate=8000" >> tmpp1.txt
+sudo mv /boot/config.txt /boot/config.txt-bak
 sudo mv tmpp1.txt /boot/config.txt
 sudo apt update
 sudo apt install xrdp tightvncserver gpsd-clients python-dev -y
